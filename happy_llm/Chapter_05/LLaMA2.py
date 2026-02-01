4568641bdd0f818d35fc45624abbd219bc5613dd
Super_Rs_7 <igxy0929@gmail.com>
2026-02-01 21:40:13 +0800

    update

--- happy_llm/Chapter_05/LLaMA2.py
+++ happy_llm/Chapter_05/LLaMA2.py
@@ -1,11 +1,14 @@
 import math
-from typing import Tuple
+from typing import Tuple, Optional
 
 import torch
 from numpy import reshape
-from transformers import PretrainedConfig
+from transformers import PretrainedConfig, PreTrainedModel
 import torch.nn as nn
 import torch.nn.functional as F
+from transformers.modeling_outputs import CausalLMOutputWithPast
+
+
 class ModelConfig(PretrainedConfig):
     model_type = "Tiny_K"
     def __init__(
@@ -113,7 +116,7 @@ def apply_rotary_emb(
         freq_cos: torch.Tensor,
         freq_sin: torch.Tensor
 ) -> Tuple[torch.Tensor, torch.Tensor]:
-    xq_r, xq_i = xq.float(),reshape(xq.shape[:-1] + (-1, 2)).unbind(-1)
+    xq_r, xq_i = xq.float().reshape(xq.shape[:-1] + (-1, 2)).unbind(-1)
     xk_r, xk_i = xk.float().reshape(xk.shape[:-1] + (-1, 2)).unbind(-1)
 
     freq_cos = reshape_for_broadcast(freq_cos, xq_r)
@@ -138,7 +141,7 @@ class Attention(nn.Module):
         model_parallel_size = 1
         self.n_local_heads = args.n_heads // model_parallel_size
         self.n_local_kv_heads = args.n_kv_heads // model_parallel_size
-        self.n_rep = self.n_local_heads // self.n_local_kvheads
+        self.n_rep = self.n_local_heads // self.n_local_kv_heads
         self.head_dim = args.dim // args.n_heads
 
         # 定义权重矩阵
@@ -150,7 +153,7 @@ class Attention(nn.Module):
 
         # 定义dropout
         self.attn_dropout = nn.Dropout(args.dropout)
-        self.attn_dropout = nn.Dropout(args.dropout)
+        self.resid_dropout = nn.Dropout(args.dropout)
         # 保存dropout概率
         self.dropout = args.dropout
 
@@ -207,6 +210,187 @@ class Attention(nn.Module):
         output = self.resid_dropout(output)
         return output
 
+'''构建MLP模块'''
+
+class MLP(nn.Module):
+    def __init__(self, dim: int, hidden_dim: int, multiple_of: int, dropout: float):
+        super().__init__()
+        # 如果没有指定隐藏层的维度，我们将其设置为输入维度的4倍
+        # 然后将其减少到2/3，最后确保它是multiple_of的倍数
+        if hidden_dim is None:
+            hidden_dim = 4 * dim
+            hidden_dim = int(2 * hidden_dim / 3)
+            hidden_dim = multiple_of * ((hidden_dim + multiple_of - 1) // multiple_of)
+        # 定义第一层线性变换，从输入维度到隐藏维度
+        self.w1 = nn.Linear(dim, hidden_dim, bias=False)
+        # 定义第二层线性变换，从隐藏维度到输入维度
+        self.w2 = nn.Linear(hidden_dim, dim, bias=False)
+        # 定义第三层线性变换，从输入维度到隐藏维度
+        self.w3 = nn.Linear(dim, hidden_dim, bias=False)
+        # 定义dropout层，用于防止过拟合
+        self.dropout = nn.Dropout(dropout)
+    def forward(self, x):
+        # 前向传播函数
+        # 首先，输入x通过第一层线性变换和SILU激活函数
+        # 然后，结果乘以输入x通过第三层线性变化的结果
+        # 最后，通过第二层线性变换和dropout层
+        return self.dropout(self.w2(F.silu(self.w1(x)) * self.w3(x)))
+
+'''LLaMA2 Decoder Layer'''
+
+class DecoderLayer(nn.Module):
+    def __init__(self, layer_id: int, args: ModelConfig):
+        super().__init__()
+        self.n_heads = args.n_heads
+        self.dim = args.dim
+        self.head_dim = args.dim // args.n_heads
+        self.attention = Attention(args)
+        self.feed_forward = MLP(
+            dim=args.dim,
+            hidden_dim=args.hidden_dim,
+            multiple_of=args.multiple_of,
+            dropout=args.dropout,
+        )
+        # 定义层ID
+        self.layer_id = layer_id
+        #定义注意力计算的归一化层
+        self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps)
+        # 定义前馈神经网络计算的归一化层
+        self.ffn_norm = RMSNorm(args.dim, eps=args.norm_eps)
+
+    def forward(self, x, freqs_cos, freqs_sin):
+        # 前向传播函数
+        # 首先，输入x经过注意力归一化层，然后进行注意力计算，结果与输入x相加得到h
+        # 然后，h经过前馈神经网络归一化层，然后进行前馈神经网络计算，结果与h相加得到输出
+        h = x + self.attention.forward(self.attention_norm(x), freqs_cos, freqs_sin)
+        out = h + self.feed_forward.forward(self.ffn_norm(h))
+
+        return out
+
+class Transformer(PreTrainedModel):
+    config_class = ModelConfig # 配置类
+    last_loss: Optional[torch.Tensor] # 记录最后一次计算的损失
+
+    def __init__(self, args: ModelConfig = None):
+        super().__init__(args)
+        # 初始化 模型参数
+        self.args = args
+        # 词汇表大小
+        self.vocab_size = args.vocab_size
+        # 层数
+        self.n_layer = args.n_layers
+
+        # 词嵌入层
+        self.tok_embeddings = nn.Embedding(args.vocab_size, args.dim)
+
+        # Dropout层
+        self.dropout = nn.Dropout(args.dropout)
+
+        # Decoder 层
+        self.layers = torch.nn.ModuleList()
+        for layer_id in range(args.n_layers):
+            self.layers.append(DecoderLayer(layer_id, args))
+
+        # 归一化层
+        self.norm = RMSNorm(args.dim, eps=args.norm_eps)
+        # 输出层
+        self.output = nn.Linear(args.dim, args.vocab_size, bias=False)
+
+        # 将词嵌入层的权重与输出层的权重共享
+        self.tok_embeddings.weight = self.output.weight
+
+        # 预计算相对位置嵌入的频率
+        freqs_cos, freqs_sin = precompute_freqs_cis(self.args.dim // self.args.n_heads, self.args.max_seq_len)
+        self.register_buffer("freqs_cos", freqs_cos, persistent=False)
+        self.register_buffer("freqs_sin", freqs_sin, persistent=False)
+
+        # 初始化所有权重
+        self.apply(self._init_weights)
+        # 对残差投影进行特殊的缩放初始化
+        for pn, p in self.named_parameters():
+            if pn.endswith('w3.weight') or pn.endswith('wo.weight'):
+                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * args.n_layers))
+
+        # 初始化最后一次前向传播的损失属性
+        self.last_loss = None
+        self.OUT = CausalLMOutputWithPast() # 输入容器
+        self._no_split_modules = [name for name, _ in self.named_modules()]
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, tokens: torch.Tensor, targets: Optional[torch.Tensor] = None, **kwargs) -> torch.Tensor:
+
+        if 'input_ids' in kwargs:
+            tokens = kwargs['input_ids']
+        if 'attention_mask' in kwargs:
+            targets = kwargs['attention_mask']
+
+        # 前向传播函数
+        _bsz, seqlen = tokens.shape
+        # 通过词嵌入和Dropout层
+        h = self.tok_embeddings(tokens)
+        h = self.dropout(h)
+        # 获取相对位置嵌入的频率
+        freqs_cos = self.freqs_cos[:seqlen]
+        freqs_sin = self.freqs_sin[:seqlen]
+
+        # 通过Decoder层
+        for layer in self.layers:
+            h = layer(h, freqs_cos, freqs_sin)
+
+        # 通过归一化层
+        h = self.norm(h)
+
+        if targets is not None:
+            # 如果给定了目标，计算损失
+            logits = self.output(h)
+            self.last_loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=0, reduction='none')
+
+        else:
+            # 推理时的小优化: 只对最后一个位置的输出进行前向传播
+            logits = self.output(h[:, [-1], :])
+            self.last_loss = None
+
+        # 设置输出
+        self.OUT.__setitem__('logits', logits)
+        self.OUT.__setitem__('last_loss', self.last_loss)
+        return self.OUT
+
+    @torch.inference_mode()
+    def generate(self, idx, stop_id=None, max_new_tokens=256, temperature=1.0, top_k=None):
+        index = idx.shape[1]
+        for _ in range(max_new_tokens):
+            idx_cond = idx if idx.size(1) <= self.args.max_seq_len else idx[:, -self.args.max_seq_len:]
+
+            logits = self(idx_cond).logits
+            logits = logits[:, -1, :]
+
+            if temperature == 0.0:
+                # 选择最有可能的索引
+                _, idx_next = torch.topk(logits, k=1, dim=-1)
+            else:
+                logits = logits / temperature
+                if top_k is not None:
+                    v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+                    logits[logits < v[: [-1]]] = -float('Inf')
+
+                probs = F.softmax(logits, dim=-1)
+                idx_next = torch.multinomial(probs, num_samples=1)
+
+            if idx_next == stop_id:
+                break
+
+            idx = torch.cat((idx, idx_next), dim=1)
+
+
+        return idx[:, index:]
 
 
 
@@ -214,15 +398,16 @@ class Attention(nn.Module):
 
 
 
+# LLaMA2Model.forward 接受两个参数，tokens和targets，其中tokens是输入的张量, 应为int类型
+x = torch.randint(0, 6144, (1, 50)) # [bs, seq_len]
+# 实例化LLaMA2Model
+model = Transformer(args=args)
+# 计算model的全部参数
+num_params = sum(p.numel() for p in model.parameters())
+print('Number of parameters:', num_params)
 
-
-
-
-
-
-
-
-
+out = model(x)
+print(out.logits.shape) # [batch_size, 1, vocab_size]
 
 
 
